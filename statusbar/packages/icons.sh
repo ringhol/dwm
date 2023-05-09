16a0b2e07bb409697df0543c3707f8e7b3f37848
semtor <semtorcc@gmail.com>
2023-05-09 13:03:53 +0800

    添加接入耳机时的图标，修改连接clash时的图标

--- statusbar/packages/icons.sh
+++ statusbar/packages/icons.sh
@@ -7,8 +7,8 @@ this=_icons
 color="^c#2D1B46^^b#5555660x66^"
 signal=$(echo "^s$this^" | sed 's/_//')
 
-with_v2raya() {
-    [ "$(ps aux | grep -v grep | grep 'v2raya')" ] && icons=(${icons[@]} "")
+with_clash() {
+    [ "$(ps aux | grep -v grep | grep 'clash-for-windows')" ] && icons=(${icons[@]} "")
 }
 
 with_bluetooth() {
@@ -17,37 +17,42 @@ with_bluetooth() {
     [ "$(bluetoothctl info 88:C9:E8:14:2A:72 | grep 'Connected: yes')" ] && icons=(${icons[@]} "")
 }
 
+with_headphone(){
+  [ ! "$(command -v amixer)" ] && echo command not found: amixer && return
+  [ "$(amixer -c 0 get Headphone|grep -o '\[on\]')" ] && icons=(${icons[@]} "󰋋")
+}
+
 update() {
-    icons=("")
-    with_v2raya
-    # with_bluetooth
-
+    icons=("󰔉")
+    with_clash
+    with_bluetooth
+    with_headphone
     text=" ${icons[@]} "
-
     sed -i '/^export '$this'=.*$/d' $tempfile
     printf "export %s='%s%s%s'\n" $this "$signal" "$color" "$text" >> $tempfile
 }
 
 notify() {
     texts=""
-    [ "$(ps aux | grep -v grep | grep 'v2raya')" ] && texts="$texts\n v2raya 已启动"
-    [ "$(bluetoothctl info 88:C9:E8:14:2A:72 | grep 'Connected: yes')" ] && texts="$texts\n WH-1000XM4 已链接"
+    [ "$(ps aux | grep -v grep | grep 'clash-for-windows')" ] && texts="$texts\n clash已启动"
+    # [ "$(bluetoothctl info 88:C9:E8:14:2A:72 | grep 'Connected: yes')" ] && texts="$texts\n WH-1000XM4 已链接"
+    [ "$(amixer -c 0 get Headphone|grep -o '\[on\]')" ] && texts="$texts\n󰋋 耳机已连接"
     [ "$texts" != "" ] && notify-send " Info" "$texts" -r 9527
 }
 
-call_menu() {
-    case $(echo -e ' 关机\n 重启\n 休眠\n 锁定' | rofi -dmenu -window-title power) in
-        " 关机") poweroff ;;
-        " 重启") reboot ;;
-        " 休眠") systemctl hibernate ;;
-        " 锁定") ~/scripts/blurlock.sh ;;
-    esac
-}
+# call_menu() {
+#     case $(echo -e ' 关机\n 重启\n 休眠\n 锁定' | rofi -dmenu -window-title power) in
+#         " 关机") poweroff ;;
+#         " 重启") reboot ;;
+#         " 休眠") systemctl hibernate ;;
+#         " 锁定") ~/scripts/blurlock.sh ;;
+#     esac
+# }
 
 click() {
     case "$1" in
-        L) notify; feh --randomize --bg-fill ~/Pictures/wallpaper/*.png ;;
-        R) call_menu ;;
+        L) notify ;;
+        R) feh --randomize --bg-fill ~/Pictures/wallpaper/*.png ;;
     esac
 }
 
