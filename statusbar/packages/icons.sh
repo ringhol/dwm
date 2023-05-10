446928381414e4cace5a293f74430177d28a83e5
semtor <semtorcc@gmail.com>
2023-05-10 14:47:20 +0800

    去除不需要的图标

--- statusbar/packages/icons.sh
+++ statusbar/packages/icons.sh
@@ -7,16 +7,6 @@ this=_icons
 color="^c#2D1B46^^b#5555660x66^"
 signal=$(echo "^s$this^" | sed 's/_//')
 
-with_clash() {
-    [ "$(ps aux | grep -v grep | grep 'clash-for-windows')" ] && icons=(${icons[@]} "")
-}
-
-with_bluetooth() {
-    # 此处为自用蓝牙设备的 MAC 地址，你可以自定义该部分
-    [ ! "$(command -v bluetoothctl)" ] && echo command not found: bluetoothctl && return
-    [ "$(bluetoothctl info 88:C9:E8:14:2A:72 | grep 'Connected: yes')" ] && icons=(${icons[@]} "")
-}
-
 with_headphone(){
   [ ! "$(command -v amixer)" ] && echo command not found: amixer && return
   [ "$(amixer -c 0 get Headphone|grep -o '\[on\]')" ] && icons=(${icons[@]} "󰋋")
@@ -24,8 +14,6 @@ with_headphone(){
 
 update() {
     icons=("󰔉")
-    with_clash
-    with_bluetooth
     with_headphone
     text=" ${icons[@]} "
     sed -i '/^export '$this'=.*$/d' $tempfile
@@ -34,7 +22,6 @@ update() {
 
 notify() {
     texts=""
-    [ "$(ps aux | grep -v grep | grep 'clash-for-windows')" ] && texts="$texts\n clash已启动"
     # [ "$(bluetoothctl info 88:C9:E8:14:2A:72 | grep 'Connected: yes')" ] && texts="$texts\n WH-1000XM4 已链接"
     [ "$(amixer -c 0 get Headphone|grep -o '\[on\]')" ] && texts="$texts\n󰋋 耳机已连接"
     [ "$texts" != "" ] && notify-send " Info" "$texts" -r 9527
