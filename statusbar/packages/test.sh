0ed8718622b51f815cf18d5025f3d897f1be2dc8
semtor <semtorcc@gmail.com>
2023-05-10 16:06:03 +0800

    添加v2raya自动获取Authorization

--- statusbar/packages/test.sh
+++ statusbar/packages/test.sh
@@ -1,35 +1,7 @@
-power_on() {
-	if bluetoothctl show | grep -q "Powered: yes"; then
-		return 0
-	else
-		return 1
-	fi
-}
-print_status() {
-	if power_on; then
-		info=' 已配对的设备
-    '
-		paired_devices_cmd="devices Paired"
-		# Check if an outdated version of bluetoothctl is used to preserve backwards compatibility
-		if (($(echo "$(bluetoothctl version | cut -d ' ' -f 2) < 5.65" | bc -l))); then
-			paired_devices_cmd="paired-devices"
-		fi
-		mapfile -t paired_devices < <(bluetoothctl $paired_devices_cmd | grep Device | cut -d ' ' -f 2)
-    if [ ${#paired_devices[@]} -eq 0 ]; then
-      info="$info 无"
-      break
-    fi
-		for device in "${paired_devices[@]}"; do
-			if device_connected $device; then
-				device_alias=$(bluetoothctl info $device | grep "Alias" | cut -d ' ' -f 2-)
-        info="$info
-        $device_alias"
-			fi
-		done
-	else
-		info='󰂲 蓝牙未开启'
-	fi
-	notify-send "$info" -r 9527
+get_authorization() {
+	resp=$(curl http://localhost:2017/api/login -X POST -d '{"username": "semtor", "password": "010107"}' -H "Content-Type: application/json")
+	TOKEN=$(echo $resp | grep -o '"token":"[^"]\+"' | cut -d '"' -f4)
+	echo $TOKEN
 }
 
-print_status
+get_authorization
--- statusbar/packages/v2ray.sh
+++ statusbar/packages/v2ray.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-authorization="Authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODYyODk1OTMsInVuYW1lIjoic2VtdG9yIn0.gCRUyqEvht4JmPR2LksuB1t0Nd7IFY7nd3qUPWqnjsE"
+_v2ray_auth=''
 
 tempfile=$(
 	cd $(dirname $0)
@@ -18,6 +18,15 @@ update() {
 	printf "export %s='%s%s%s'\n" $this "$signal" "$color" "$icon" >>$tempfile
 }
 
+get_authorization() {
+	resp=$(curl http://localhost:2017/api/login -X POST -d '{"username": "semtor", "password": "010107"}' -H "Content-Type: application/json")
+	_v2ray_auth=$(echo $resp | grep -o '"token":"[^"]\+"' | cut -d '"' -f4)
+	sed -i '/^export '_v2ray_auth'=.*$/d' $tempfile
+	printf "export %s='%s'\n" _v2ray_auth "$_v2ray_auth" >>$tempfile
+}
+
+
+
 notify() {
 	line1=" v2ray"
 	text=" v2ray未启动，老实在家待着吧"
@@ -27,12 +36,15 @@ notify() {
 
 toggle_clash() {
 	line1=" v2ray"
+  source $tempfile
+  [ -z "$_v2ray_auth" ] && get_authorization
+
 	if [ "$(ps -ef | grep -v grep | grep 'v2ray run')" ]; then
 		text=" 正在关闭v2ray!"
-		curl http://127.0.0.1:2017/api/v2ray -H "$authorization" -X DELETE &
+		curl http://127.0.0.1:2017/api/v2ray -H "Authorization: $_v2ray_auth" -X DELETE &
 	else
 		text=" 正在打开v2ray!"
-		curl http://127.0.0.1:2017/api/v2ray -H "$authorization" -X POST &
+		curl http://127.0.0.1:2017/api/v2ray -H "Authorization: $_v2ray_auth" -X POST &
 	fi
 	notify-send "$line1" "$text" -r 9527
 }
